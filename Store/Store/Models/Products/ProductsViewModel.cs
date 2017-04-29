382c6d8666fb98eedc72214197c1f7eeedeebc10
Mario Slavov <hellsflash@gmail.com>
2017-04-29 11:01:25 +0300

    update

---
+++ Store/Store/Models/Products/ProductsViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Store.Models.Products
+{
+    public class ProductsViewModel
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+
+        public string Categorie { get; set; }
+
+
+        public string Description { get; set; }
+
+
+        public decimal Price { get; set; }
+
+
+        public string ImageUrl { get; set; }
+
+        
+
+    }
+}
